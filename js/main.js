5f684660c6449fd8c1e1cf21373884377b71ddd3
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-05-27 19:11:14 -0400

    Make add book function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -6,4 +8,14 @@ function Book(title, author, pages, read) {
   this.info = function() {
     return `${title} by ${author}, ${pages} pages, ${read}`
   }
+}
+
+function addBookToLibrary() {
+  const book = new Book(
+    title = prompt("Title"),
+    author = prompt("Author"),
+    pages = prompt("Pages"),
+    read = prompt("Read")
+  );
+  myLibrary.push(book);
 }
