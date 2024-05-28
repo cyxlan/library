f13feb6b51c1e012d3c96ca94797f65fe2025be5
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-05-27 20:00:13 -0400

    Display books on page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const bookTable = document.querySelector("#book-table");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -19,3 +20,48 @@ function addBookToLibrary() {
   );
   myLibrary.push(book);
 }
+
+// display books on page
+function displayBooks() {
+  const cols = ["title", "author", "pages", "read"];
+  myLibrary.forEach((book) => {
+    const row = document.createElement("tr");
+    for (const i in cols) {
+      const cell = document.createElement("td");
+      const col = cols[i]
+      if (col === "read") {
+        // if true, map to "Yes", else "No"
+        cell.textContent = book[col] ? "Yes" : "No";
+      } else {
+        cell.textContent = book[col];
+      }
+      row.appendChild(cell);
+    }
+    bookTable.appendChild(row);
+  })
+}
+
+// books for testing
+let theHobbit = new Book(
+  title = "The Hobbit",
+  author = "J.R.R. Tolkien",
+  pages = 295,
+  read = false
+);
+let jungleBook = new Book(
+  title = "The Jungle Book",
+  author = "Rudyard Kipling",
+  pages = 277,
+  read = true
+);
+let cuckoosNest = new Book(
+  title = "One Flew Over the Cuckoo's Nest",
+  author = "Ken Kesey",
+  pages = 325,
+  read = true
+);
+myLibrary.push(theHobbit);
+myLibrary.push(jungleBook);
+myLibrary.push(cuckoosNest);
+
+displayBooks();
