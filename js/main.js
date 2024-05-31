f2edc4aba10ea701b3b3e49fab05b168340880af
cyxlan <49080055+cyxlan@users.noreply.github.com>
2024-05-31 15:31:19 -0400

    Remove unneeded book info function

--- js/main.js
+++ js/main.js
@@ -8,9 +8,6 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${read}`
-  }
 }
 
 const library = {
